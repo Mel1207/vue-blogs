81024e0de498c47feaf8bb54e4c42a91de29c142
Mel <meljohnpualon@gmail.com>
2022-03-06 20:28:32 +0800

    added filtering method

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home2View from '../views/Home2Vue.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home2View
   }
 ]
 
---
+++ src/views/Home2Vue.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home">
+        <h1>Home</h1>
+        <input type="text" v-model="search">
+        <p>search term - {{ search }}</p>
+        <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    </div>
+</template>
+
+<script>
+import { computed, ref } from 'vue'
+
+export default {
+    name: 'Home2',
+    setup() {
+        const search = ref('')
+        const names = ref(['Mel', 'John', 'Doe', 'David', 'Johnson', 'Sun'])
+        
+        const matchingNames = computed(() => {
+            return names.value.filter(name => name.includes(search.value))
+        })
+
+        return { names, search, matchingNames }
+    }
+}
+</script>
