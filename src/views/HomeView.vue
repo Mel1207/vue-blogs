2af1d8b784e7889b81f70b39414a5f008785000d
Mel <meljohnpualon@gmail.com>
2022-03-06 20:13:23 +0800

    added ref vs reactive datas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,36 @@
 <template>
   <div class="home">
-    <h1>Hey there!</h1>
+    <h1>HOME</h1>
+    <h2>REFS</h2>
+    <p>{{ personOne.name }} - {{ personOne.age}}</p>
+    <button @click="updatePersonOne">update person 1</button>
+    <h2>REACTIVE</h2>
+    <p>{{ personTwo.name }} - {{ personTwo.age }}</p>
+    <button @click="updatePersonTwo">update person 2</button>
   </div>
 </template>
 
 <script>
+import { ref, reactive } from '@vue/reactivity'
 
 export default {
   name: 'HomeView',
-  components: {}
+  components: {},
+  setup() {
+    const personOne = ref({ name: 'Mel', age: 26})
+    const personTwo = reactive({ name: 'John', age: 22})
+
+    const updatePersonOne = () => {
+      personOne.value.age = 27
+    }
+
+     const updatePersonTwo = () => {
+      personTwo.age = 45
+    }
+
+    return {
+      personOne, updatePersonOne, personTwo, updatePersonTwo
+    }
+  }
 }
 </script>
